066f202b4f13d63a28258c922f93025583556af0
Vineet072 <vineetgoel003@gmail.com>
2026-04-24 23:23:46 +0530

    Tokenizer final

--- Tokenizer/Tokenizer.py
+++ Tokenizer/Tokenizer.py
@@ -4,3 +4,14 @@
 # .venv\Scripts\activate
 
 # pip install --upgrade transformers
+
+# TikTokenizer website can be taken for reference for better understanding
+
+# The model I used is open access, so no need to use hugging face token :)
+
+import os
+from transformers import AutoTokenizer
+
+modelId="google/gemma-3-1b-it"
+tokenizer=AutoTokenizer.from_pretrained(modelId)
+print(tokenizer("Hello goel"))
